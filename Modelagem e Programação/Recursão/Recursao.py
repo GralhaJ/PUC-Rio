9976386977849b0b7447de402ee7ab7ce93b03d9
GralhaJ <129180241+GralhaJ@users.noreply.github.com>
2023-05-18 10:03:34 -0300

    Update Recursao.py

--- Modelagem e Programação/Recursão/Recursao.py
+++ Modelagem e Programação/Recursão/Recursao.py
@@ -15,6 +15,32 @@ def fibonacci(x):
         return 1
     return fibonacci(x-1) + fibonacci(x-2)
 
+def qntdAlg(n):
+    if n < 10:
+        return 1
+    return 1 + qntdAlg(n//10)
+
+def qntdAlgPares(n):
+    if n < 10:
+        if n % 2 == 0:
+            return 1
+        return 0
+    if n % 2 == 0:
+        return 1 + qntdAlgPares(n // 10)
+    return qntdAlgPares(n // 10)
+    
+def exibeBomDia(n):
+    if n == 0:
+        return
+    print('Bom dia!')
+    return exibeBomDia(n-1)
+
+def exibeMsn(msn,n):
+    if n == 0:
+        return
+    print(msn)
+    return exibeMsn(msn,n-1)
+
 
 
 
