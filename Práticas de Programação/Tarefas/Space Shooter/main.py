1c354f5b1b4b5e8fb50aabe93799209352ceb212
GralhaJ <129180241+GralhaJ@users.noreply.github.com>
2023-06-25 11:17:17 -0300

    Update main.py

--- Práticas de Programação/Tarefas/Space Shooter/main.py
+++ Práticas de Programação/Tarefas/Space Shooter/main.py
@@ -76,6 +76,9 @@ def main():
     level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 20)
+    lost_font = pygame.font.SysFont("comicsans", 40)
+
+
 
     enemies = []
     wave_length = 5
@@ -87,52 +90,73 @@ def main():
 
     clock = pygame.time.Clock()
 
-    def redraw_window():
-        WIN.blit(BG, (0,0))
-        #desenhando o texto
-        lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
-        level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
+    lost = False
+    lost_count = 0
+
+def redraw_window():
+         WIN.blit(BG, (0,0))
+         #desenhando o texto
+         lives_label = main_font.render(f"Lives: {lives}", 1, (255, 255, 255))
+         level_label = main_font.render(f"Level: {level}", 1, (255, 255, 255))
         
-        WIN.blit(lives_label, (10, 10))
-        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+         WIN.blit(lives_label, (10, 10))
+         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
-        for enemy in enemies:
-             enemy.draw(WIN)
+         for enemy in enemies:
+           enemy.draw(WIN)
+
+           player.draw(WIN)
         
-        player.draw(WIN)
+         if lost:
+             lost_label = lost_font.render("Você Perdeu!", 1, (255, 255, 255))
+             WIN.blit(lost_label, (WIDTH/2 - lost_label.get_width()/2, 300))
 
-        pygame.display.update()
+             pygame.display.update()
 
-    while run:
-        clock.tick(FPS)
+         while run:
+             clock.tick(FPS)
+             redraw_window()
 
-        if len(enemies) == 0:
-             level += 1
-             wave_length += 5
-             for i in range(wave_length):
-                enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
-                enemies.append(enemy)
+         if lives <= 0 or player.health <= 0:
+             lost = True
+             lost_count += 1
 
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT:
+         if lost:
+            if lost_count > FPS * 3:
                 run = False
+            else:
+                continue
+                    
+          
+         if len(enemies) == 0:
+              level += 1
+              wave_length += 5
+              for i in range(wave_length):
+                   enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
+                   enemies.append(enemy)
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] and player.x - player_vel > 0:
-             player.x -= player_vel
-        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH:
-             player.x += player_vel
-        if keys[pygame.K_w] and player.y - player_vel > 0:
-             player.y -= player_vel
-        if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT:
-             player.y += player_vel
+         for event in pygame.event.get(): 
+              if event.type == pygame.QUIT:
+                   run = False
 
-        for enemy in enemies:
-             enemy.move(enemy_vel)
-             
+         keys = pygame.key.get_pressed()
+         if keys[pygame.K_a] and player.x - player_vel > 0:
+              player.x -= player_vel
+         if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH:
+              player.x += player_vel
+         if keys[pygame.K_w] and player.y - player_vel > 0:
+              player.y -= player_vel
+         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT:
+              player.y += player_vel
+
+         for enemy in enemies[:]:
+               enemy.move(enemy_vel)
+               if enemy.y + enemy.get_height() > HEIGHT:
+                   lives -= 1
+                   enemies.remove(enemy)
 
 
-        redraw_window()
 
 
 main()
+
