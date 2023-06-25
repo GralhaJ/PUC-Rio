e6ace0ddd67ad91864c5fb3de3c65d3222af66be
GralhaJ <129180241+GralhaJ@users.noreply.github.com>
2023-06-25 10:17:45 -0300

    Update main.py

--- Práticas de Programação/Tarefas/Space Shooter/main.py
+++ Práticas de Programação/Tarefas/Space Shooter/main.py
@@ -10,9 +10,9 @@ pygame.display.set_caption("Space Shooter")
 
 #carregando as imagens
 
-RED_SPACE_player = pygame.image.load("pixel_ship_red_small.png")
-GREEN_SPACE_player = pygame.image.load("pixel_ship_green_small.png")
-BLUE_SPACE_player = pygame.image.load("pixel_ship_blue_small.png")
+RED_SPACE_SHIP = pygame.image.load("pixel_ship_red_small.png")
+GREEN_SPACE_SHIP = pygame.image.load("pixel_ship_green_small.png")
+BLUE_SPACE_SHIP = pygame.image.load("pixel_ship_blue_small.png")
 
 #player
 YELLOW_SPACE_player = pygame.image.load("pixel_ship_yellow.png")
@@ -55,12 +55,31 @@ class Player(Ship):
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
 
+class Enemy(Ship):
+    COLOR_MAP = {
+                "red": (RED_SPACE_SHIP, RED_LASER),
+                "green": (GREEN_SPACE_SHIP, GREEN_LASER),
+                "blue": (BLUE_SPACE_SHIP, BLUE_LASER)
+                }
+
+    def __init__(self, x, y, color, health = 100):
+        super().__init__(x, y, health)
+        self.ship_img, self.laser_img = self.COLOR_MAP[color]
+        self.mask = pygame.mask.from_surface(self.ship_img)
+    
+    def move(self, vel):
+         self.y += vel
+
 def main():
     run = True
     FPS = 60
-    level = 1
+    level = 0
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 20)
+
+    enemies = []
+    wave_length = 5
+    enemy_vel = 1
 
     player_vel = 5
 
@@ -77,15 +96,24 @@ def main():
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
-        player.draw(WIN)
+        for enemy in enemies:
+             enemy.draw(WIN)
         
+        player.draw(WIN)
+
         pygame.display.update()
 
     while run:
         clock.tick(FPS)
-        redraw_window()
 
-        for event in pygame.event.get():
+        if len(enemies) == 0:
+             level += 1
+             wave_length += 5
+             for i in range(wave_length):
+                enemy = Enemy(random.randrange(50, WIDTH - 100), random.randrange(-1500, -100), random.choice(["red", "blue", "green"]))
+                enemies.append(enemy)
+
+        for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
                 run = False
 
@@ -99,7 +127,12 @@ def main():
         if keys[pygame.K_s] and player.y + player_vel + player.get_height() < HEIGHT:
              player.y += player_vel
 
+        for enemy in enemies:
+             enemy.move(enemy_vel)
+             
+
+
+        redraw_window()
 
 
 main()
-
